187302b68ebdd4347d2b9ebf865ee040e8027acd
Nicolas <nvalentim824@gmail.com>
2025-06-21 23:04:11 -0300

    Commit dos Cod sobre Estrutura de Controle if-else

---
+++ Instrucao_If_Else/Program.cs
@@ -0,0 +1,48 @@
+﻿Console.WriteLine("## Intrucao If e Else ##\n");
+
+// Console.WriteLine("Informe a nota do Aluno: \n");
+// var nota = Convert.ToInt32(Console.ReadLine());
+
+// if (nota > 5)
+// {
+// Console.WriteLine("Aluno aprovado!");
+// }
+// else
+// {
+// Console.WriteLine("Aluno reprovado!");
+// }
+
+// Console.WriteLine("\nFim do processamento...");
+
+// Comparar se X e maior, menor ou igual a y com if-else
+
+Console.WriteLine("Defina um valor a X: \n");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("\tDefina um valor a Y: \n");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > y)
+{
+    Console.WriteLine("X e maior que Y");
+}
+else
+{
+    if (x < y)
+    {
+        Console.WriteLine("Y e maior que X");
+    }
+    else
+    {
+        Console.WriteLine("X e igual a Y");
+    }
+}
+
+
+
+
+
+
+
+
+    Console.ReadKey();
--- Intrucao_If/Program.cs
+++ Intrucao_If/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("## Instrucao de controle If ##\n");
+
+// Console.WriteLine("Cliente Especial S/N \n");
+// var resposta = Console.ReadLine();
+
+// if (resposta == "S")
+// {
+// Console.WriteLine("Desconte de 10%");
+// }
+// Console.WriteLine("Fim do processamento");
+
+//Parte 2
+
+// Console.WriteLine("Cliente Especial True/false");
+// var resposta = Convert.ToBoolean(Console.ReadLine());
+
+// if (resposta)
+
+// Console.WriteLine("Desconto de 10%");
+
+// Console.WriteLine("Fim do processamento!");
+
+//Parte 3
+
+Console.WriteLine("Informe o valor de x: \t");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Informe valor de y: \t");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > y)
+{
+    Console.WriteLine("X e maior que Y");
+}
+
+if (x < y)
+{
+    Console.WriteLine("X e menor que Y");
+}
+
+if (x == y)
+{
+    Console.WriteLine("X e igual a Y");
+}
+
+Console.WriteLine("Fim do processamento.");
+
+Console.ReadKey();
